1d1ccdc30c161e81e28f6d22e2baf30ee39c9574
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2024-09-28 00:11:40 -0300

    index principal

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'vendor/autoload.php';
-require_once 'src/Routes/api.php';
+require_once '../vendor/autoload.php';
+require_once '../src/Routes/api.php';
 
 use GMath\Core\Core;
 use GMath\Http\Route;
