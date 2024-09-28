076bc6cac21a10bf1acc55aa00c335470288f7cd
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2024-09-28 00:03:07 -0300

    núcleo essencial do mvc

---
+++ src/Core/Core.php
@@ -0,0 +1,54 @@
+<?php 
+
+namespace GMath\Core;
+
+use Dotenv\Util\Regex;
+use GMath\Http\Request;
+use GMath\Http\Response;
+
+class Core {
+    public static function dispatch(array $routes)
+    {
+        $url = $_SERVER['REQUEST_URI'];
+        $matches = [];
+
+        $url !== '/' && $url = rtrim($url, '/');
+
+        $routeFound = false;
+        $prefixController = 'GMath\\Controllers\\';
+
+        foreach ($routes as $route) {
+            $pattern = '#^'. preg_replace('/{id}/', '([\w-]+)', $route['path']) .'$#';
+
+            if (preg_match($pattern, $url, $matches)) {
+                array_shift($matches);
+
+                $routeFound = true;
+
+                if ($route['method'] != Request::method()){
+                    Response::json([
+                        'erro' => true,
+                        'success' => false,
+                        'message' => "Method not allowed",
+                    ], 405);
+                    return;
+                }
+
+                [$controller, $action] = explode('@', $route['action']);
+
+                $controller = $prefixController . $controller;
+
+                $extendController = new $controller();
+                $extendController->$action(new Request, new Response, $matches);
+            }
+        }
+
+        if (!$routeFound){
+            if (!$routeFound) {
+                $controller = $prefixController . 'NotFoundController';
+                $extendController = new $controller();
+                $extendController->index(new Request, new Response);
+            }
+        }
+    }
+}
